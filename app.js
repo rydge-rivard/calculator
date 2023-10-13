fa7a7642f9a3ae08a1cdc3ba1f127f0f35c2fd06
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-13 21:29:36 +0100

    Create basic calculator functions and variables.

---
+++ app.js
@@ -0,0 +1,20 @@
+let x;
+let y;
+let operator;
+
+function add(x, y) {
+    return x + y;
+} 
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
