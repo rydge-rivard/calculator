6e40cf1d7e9e35aca3f4c70854437c6ce6130bba
Rydge Rivard <96926831+rydge-rivard@users.noreply.github.com>
2023-10-16 08:51:48 +0100

    Add computation logic functions, clear button, and equals.

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let x = 1;
-let y = 1;
+let x = 0;
+let y = 0;
 let operator = '';
 let displayNumbers = 0;
 
 function add(x, y) {
-    return x + y;
+    console.log(typeof(x));
+    console.log(typeof(y));
+    return +x + +y;
 } 
 
 function subtract(x, y) {
@@ -21,22 +23,49 @@ function divide(x, y) {
 
 function operate(x, y, operator) {
     if (operator === '+') {
-        return add(x, y);
+        displayNumbers = add(x, y);
     } else if (operator === '-') {
-        return subtract(x, y);
-    } else if (operator === '*') {
-        return multiply(x, y);
+        displayNumbers = subtract(x, y);
+    } else if (operator === 'x') {
+        displayNumbers = multiply(x, y);
     } else if (operator === '/') {
-        return divide(x, y);
+        displayNumbers = divide(x, y);
     }
+    return displayDOM.textContent = displayNumbers;
 }
 
-const displayDOM = document.querySelector('.display')
-const btn = document.querySelectorAll('.number')
+const displayDOM = document.querySelector('.display');
+const btn = document.querySelectorAll('.number');
 btn.forEach((singleButton) => {
     singleButton.addEventListener('click', () => {
         displayNumbers === 0 ? 
         displayNumbers = singleButton.textContent : displayNumbers += singleButton.textContent;
-        displayDOM.textContent = displayNumbers
-    })
+        displayDOM.textContent = displayNumbers;
+    });
+});
+
+const btnOperator = document.querySelectorAll('.operator');
+btnOperator.forEach((singleOperator) => {
+    singleOperator.addEventListener('click', () => {
+        x = displayNumbers;
+        //x !== 0 ? y = displayNumbers : x = displayNumbers;
+        displayNumbers = 0;
+        operator = singleOperator.textContent;
+        //x && y !== 0 ? operate(x, y, operator) : console.log(operate(x, y, operator));
+    });
+});
+
+const btnEquals = document.querySelector('.equals');
+btnEquals.addEventListener('click', () => {
+    x !== 0 ? y = displayNumbers: x = displayNumbers;
+    operate(x, y, operator);
+});
+
+const btnClear = document.querySelector('.clear');
+btnClear.addEventListener('click', () => {
+    displayNumbers = 0;
+    displayDOM.textContent = displayNumbers;
+    x = 0;
+    y = 0;
+    operator = '';
 });
